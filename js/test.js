1cdd46ea57e2a84482e9a24708da52cd31c598c4
salager34 <remy.salager@gmail.com>
2014-07-01 12:16:46 +0200

    test.js

--- js/test.js
+++ js/test.js
@@ -1,6 +1,15 @@
 
                     
-   function c_showInMenu(c_showInMenu){
-     if (c_showInMenu==1)
-     $(div).append(categories);
+   function affichage(categrorie){
+     if (categorie.c_showInMenu==1)
+     $(#new_div().id).append(categorie.name);
    }
+
+    function new_div()
+    {
+     htmldiv = document.createElement("div");
+     htmldiv.id = 'id_de_la_div';
+     htmldiv.className = 'nouvelle';
+     htmldivinnerHTML = document.getElementById('zonetxt').value;
+     document.getElementById("cadre").appendChild(maDiv);
+    }
