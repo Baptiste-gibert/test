1eed10dc2b07c30f35ddcf55aba17206a8cce59c
Xiaofan WU <xiaofan.wu@plebicom.com>
2014-07-02 14:34:46 +0200

    essai

--- js/test.js
+++ js/test.js
@@ -18,24 +18,8 @@ $(document).ready(function() {
                 $.each(this, function(k , v) {
                     tbl_body += "<tr>"+tbl_row+"</tr>";
                 })
-
             });
-
             $("#table").html(tbl_body);
         });
     });
 });
-
-// var url: "http://www.quiksilver.fr/s/QS-FR/dw/shop/v13_1/categories/root?levels=2&client_id=a76723ac-440f-4814-bd34-d492a140d78e",
-
-// $.getJSON(url , function(data) {
-//     var tbl_body = "";
-//     $.each(data, function() {
-//         var tbl_row = "";
-//         $.each(this, function(k , v) {
-//             tbl_row += "<td>"+v+"</td>";
-//         })
-//         tbl_body += "<tr>"+tbl_row+"</tr>";                 
-//     })
-//     $("#target_table_id tbody").html(tbl_body);
-// });
