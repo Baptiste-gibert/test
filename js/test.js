c11a1074b85285d136a5293e8ad8964837718a3b
Xiaofan WU <xiaofan.wu@plebicom.com>
2014-07-02 16:54:51 +0200

    affiche

--- js/test.js
+++ js/test.js
@@ -1,25 +1,29 @@
 $(document).ready(function() {
     $.ajax({
+        type: "POST",
         url: "http://www.quiksilver.fr/s/QS-FR/dw/shop/v13_1/categories/root?levels=2&client_id=a76723ac-440f-4814-bd34-d492a140d78e",
-        dataType: "text",
+        dataType: "jsonp",
         success: function(data) {
-            var json = $.parseJSON( data );
+            // console.log(data);
+            var tbl = '<div class="row">';
+            var j = 1;
+            $.each(data.categories, function(cat) {
+                if ( data.c_showInMenu === 1 ) {
+                    for ( var i=0, len=data.categories.length; i<len; i++) {
+                        // console.log(data.categories[i].id);
+                        tbl=tbl+'<div class="col-lg-3"><div class="col-md-12 parent-cat">'+data.categories[i].id +'</div>'+'Subcatecories' + '</div>';
+                        
 
-            var tbl_row = "";
-            $.each( json, function() {
-
-                if ( json.c_showInMenu === 0 ) {
-                tbl_row += "<td>"+json.categories;
-                    if ( json.c_showInMenu === 1 ) {
-                        tbl_row += "<td>"+json.categories+"</td>";
+                        if ( j % 4 === 0 ) {
+                            tbl+='</div><div class="row">';
+                            console.log(i);
+                        }
+                        j++;
                     }
-                    else tbl_row+="</td>";
                 }
-                $.each(this, function(k , v) {
-                    tbl_body += "<tr>"+tbl_row+"</tr>";
-                })
             });
-            $("#table").html(tbl_body);
-        });
+            tbl+='</div>';
+            $(".container-fluid").append(tbl);
+        }
     });
 });
